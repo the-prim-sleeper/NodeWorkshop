8f2325eb04633f65c93c80353cd642de0c9480d1
jesse <mr.bossman075@gmail.com>
2020-10-11 20:50:12 -0400

    first commit 2020-10-11

---
+++ backend.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+
+
+
+app.get('/', (req, res, next) => {
+  res.sendfile('./leekspin.com.html');
+})
+app.get('/image', (req, res, next) => {
+  res.sendfile('./image.gif');
+})
+app.get('/polkka.mp3', (req, res, next) => {
+  res.sendfile('./polkka.mp3');
+})
+app.get('/info.txt', (req, res, next) => {
+  res.sendfile('./info.txt');
+})
+app.get('/polkka.ogg', (req, res, next) => {
+  res.sendfile('./polkka.ogg');
+})
+app.get('/icon.png', (req, res, next) => {
+  res.sendfile('./icon.png');
+})
+app.listen(8000);
